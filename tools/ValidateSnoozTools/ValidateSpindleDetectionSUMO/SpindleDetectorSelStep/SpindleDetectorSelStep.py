eb43874141f239adb22340756d5ce104977f63d5
boshrakh95 <boshra.khajehpiri.1@ens.etsmtl.ca>
2025-06-20 15:15:54 -0400

    Fixed input validator regex to correctly accept dash, underscore, spaces, and Latin1 characters

--- tools/ValidateSnoozTools/ValidateSpindleDetectionSUMO/SpindleDetectorSelStep/SpindleDetectorSelStep.py
+++ tools/ValidateSnoozTools/ValidateSpindleDetectionSUMO/SpindleDetectorSelStep/SpindleDetectorSelStep.py
@@ -37,7 +37,7 @@ class SpindleDetectorSelStep( BaseStepView,  Ui_SpindleDetectorSelStep, QtWidget
         # Set input validators
         # Create a QRegularExpressionValidator with the desired regular expression
         # Regular expression for alphanumeric, space, dash, and latin1 (ISO/CEI 8859-1) characters
-        regex = QRegularExpression(r'^[a-zA-Z0-9ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿ\s-_]*$')
+        regex = QRegularExpression(r'^[a-zA-Z0-9ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿ\s_-]*$')
         validator = QRegularExpressionValidator(regex)
 
         # added
